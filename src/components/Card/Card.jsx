8cf99592cdd9f4d8f4e3e17333f4ceac91b786e8
albina990 <albina.khadieva90@gmail.com>
2023-08-22 08:51:32 +0200

    fix flashcards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,24 +3,20 @@ import st from "./Card.module.scss"
 import Button from "../Button/Button"
 
 export default function Card(props) {
-    const { item, styleName, index } = props
+    const { item, styleName, index, childIndex, onChildClick} = props
     const [selectedItem, setSelectedItem] = useState(false)
-    const [selectedIndex, setSelectedIndex] = useState(false)
 
     const handleClick = () => {
         setSelectedItem(!selectedItem)
     }
-    const handleChange = (index) => {
-        setSelectedIndex(index)
+
+    const handleChange = () => {
+        onChildClick(index);
     }
 
     useEffect(() => {
         setSelectedItem(false)
     }, [props.item])
-
-    useEffect(() => {
-        setSelectedIndex(false)
-    }, [props.index])
 
     if (styleName === "slider__card") {
         return (
@@ -52,10 +48,10 @@ export default function Card(props) {
                 <div className={st.flashcards__card} key={index}>
                     <h3 className={st.flashcard__word}>{item.english}</h3>
                     <p>{item.transcription}</p>
-                    {index === selectedIndex ? (
+                    {index === childIndex? (
                         <p>{item.russian}</p>
                     ) : (
-                        <p onClick={() => handleChange(index)}>
+                        <p onClick={() => handleChange()}>
                             <Button
                                 styleName="flashcards__btn"
                                 content="Показать"
--- src/pages/Cards/Cards.jsx
+++ src/pages/Cards/Cards.jsx
@@ -3,12 +3,17 @@ import st from "./Cards.module.scss"
 import Card from "../../components/Card/Card"
 
 export default function Cards(props) {
+    const [childIndex, setChildIndex] = useState(null);
+
+    const handleChildClick = (index) => {
+        setChildIndex(index);
+      }
 
     return (
         <>
-            <div className={st.container}>
+            <div className={st.container} >
                 {props.arr.map((word, index) => (
-                    <Card item={word} index={index} styleName="flashcards__card" />
+                    <Card item={word} index={index} childIndex={childIndex} onChildClick={handleChildClick} styleName="flashcards__card" />
                 ))}
             </div>
         </>
